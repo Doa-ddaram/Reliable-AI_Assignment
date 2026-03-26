5d25b15cceb10c52da36cf57ff1806dfaf605bd1
doa-ddaram <sqoml123@naver.com>
2026-03-26 08:35:44 +0000

    Update attack bug report

--- adversarial_attack.py
+++ adversarial_attack.py
@@ -30,13 +30,11 @@ def fgsm_targeted(model, x, target, eps):
 def fgsm_untargeted(model, x, true_label, eps):
     '''
     model       : the neural network
-    x           : input image tensor (requires_grad should be set)
+    x           : input image tensor
     true_label  : the correct class label
-    eps         : perturbation magnitude (e.g., 0.1, 0.3)
+    eps         : perturbation magnitude 
     return      : adversarial image x_adv
     '''
-    
-    # Set requires_grad attribute of tensor. Important for Attack
     x.requires_grad = True
     
     model.zero_grad()  # Zero all existing gradients
@@ -68,21 +66,24 @@ def pgd_targeted(model, x, target, k, eps, eps_step):
     x_adv = x.clone().detach()
     
     for i in range(k):
+        x_adv = x_adv.detach() # Detach from previous graph to prevent gradient accumulation
         x_adv.requires_grad = True
-        
+
         model.zero_grad()  # Zero all existing gradients
         output = model(x_adv)  # Forward pass
         loss = nn.CrossEntropyLoss()(output, target)  # Calculate loss with respect to target label
         loss.backward()  # Backward pass to calculate gradients of model parameters and input  
         
-        # Collect the sign of the gradients of the input image
-        sign_data_grad = x_adv.grad.data.sign()
-        # Create the perturbed image by adjusting each pixel of the input image
-        x_adv = x_adv - eps_step * sign_data_grad  # Subtract because it's a targeted attack
-        
-        # Adding clipping to maintain [0,1] range and ensure perturbation is within eps ball
-        x_adv = torch.clamp(x_adv, x - eps, x + eps)
-        x_adv = torch.clamp(x_adv, 0, 1)
+        with torch.no_grad():
+            
+            # Collect the sign of the gradients of the input image
+            sign_grad = x_adv.grad.sign()
+            # Create the perturbed image by adjusting each pixel of the input image
+            x_adv = x_adv - eps_step * sign_grad # Subtract because it's a targeted attack
+            
+            # Adding clipping to maintain [0,1] range and ensure perturbation is within eps ball
+            x_adv = torch.clamp(x_adv, x - eps, x + eps)
+            x_adv = torch.clamp(x_adv, 0, 1)
     
     return x_adv
 
@@ -101,21 +102,22 @@ def pgd_untargeted(model, x, true_label, k, eps, eps_step):
     x_adv = x.clone().detach()
     
     for i in range(k):
-        x_adv.requires_grad = True
         
+        x_adv.requires_grad = True
         model.zero_grad()  # Zero all existing gradients
         output = model(x_adv)  # Forward pass
         loss = nn.CrossEntropyLoss()(output, true_label)  # Calculate loss with respect to true label
         loss.backward()  # Backward pass to calculate gradients of model parameters and input  
         
-        # Collect the sign of the gradients of the input image
-        sign_data_grad = x_adv.grad.data.sign()
-        # Create the perturbed image by adjusting each pixel of the input image
-        x_adv = x_adv + eps_step * sign_data_grad  # Add because it's an untargeted attack
-        
-        # Adding clipping to maintain [0,1] range and ensure perturbation is within eps ball
-        x_adv = torch.clamp(x_adv, x - eps, x + eps)
-        x_adv = torch.clamp(x_adv, 0, 1)
+        with torch.no_grad():
+            # Collect the sign of the gradients of the input image
+            sign_grad = x_adv.grad.sign()
+            # Create the perturbed image by adjusting each pixel of the input image
+            x_adv = x_adv + eps_step * sign_grad  # Add because it's an untargeted attack
+
+            # Adding clipping to maintain [0,1] range and ensure perturbation is within eps ball
+            x_adv = torch.clamp(x_adv, x - eps, x + eps)
+            x_adv = torch.clamp(x_adv, 0, 1)
     
     return x_adv
 
